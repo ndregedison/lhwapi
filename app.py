e68b420adce66b99d94aa6c3120674457180dc6b
Edison <ndregedison@gmail.com>
2018-04-09 07:07:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ reload(sys)
 sys.setdefaultencoding("utf-8")
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost/lhwApi'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql://postgres:root@localhost/lhwApi')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
